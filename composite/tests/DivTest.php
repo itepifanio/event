d1eaa792780ae5a20a6e44590299dcfcae70e7d2
Iolanda Chagas <iccpaiva@gmail.com>
2020-10-02 18:38:45 -0300

    FIX: modifica divTest

--- composite/tests/DivTest.php
+++ composite/tests/DivTest.php
@@ -14,16 +14,19 @@ class DivTest extends TestCase
     /** @test */
     public function it_can_render_form()
     {
-        $div = new Div('teste'); //id, name
+        $div = new Div('teste');
 
-        $form = new Form();   //id, name
-        $form->addElement(new Label('Test')); //text, id, name
-        $form->addElement(new Input('text', null, 'test', )); //type, id, name
+        $form = new Form();
+        $form->addElement(new Label('Test'));
+        $form->addElement(new Input('text', null, 'test', ));
 
+        $div2 = new Div();
+        $div2->addElement(new Label('Test 2'));
+        $div->addElement($div2);
         $div->addElement($form);
 
         $this->assertSame(
-            '<div id="teste"><form><label>Test</label><input type="text" name="test"/></form></div>',
+            '<div id="teste"><div><label>Test 2</label></div><form><label>Test</label><input type="text" name="test"/></form></div>',
             $div->render()
         );
     }
