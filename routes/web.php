69e6af58228c0764b7da2b976c4ea34643d43774
Iolanda Chagas <iccpaiva@gmail.com>
2020-10-09 22:47:39 -0300

    FIX: modifica chamada do controller de subscription nas rotas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrganizationController;
+use App\Http\Controllers\SubscriptionsController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\HomeController;
 
@@ -19,9 +20,8 @@ Route::middleware(['auth'])->group(function(){
             'as' => 'organizations',
         ]);
     });
-
     Route::group(['prefix' => 'events/{event}'], function(){
-        Route::resource('subscription', 'SubscriptionsController', [
+        Route::resource('subscription', SubscriptionsController::class, [
             'as' => 'events',
         ]);
     });
