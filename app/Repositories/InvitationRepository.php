e7b6e0644a1bc086789593b21e4063ee1ace0597
Iolanda Chagas <iccpaiva@gmail.com>
2020-10-31 19:58:03 -0300

    FIX: remove comentários desnecessários

--- app/Repositories/InvitationRepository.php
+++ app/Repositories/InvitationRepository.php
@@ -5,14 +5,12 @@ namespace App\Repositories;
 
 use App\Models\Confirmation;
 use Illuminate\Support\Facades\DB;
-// use Illuminate\Support\Facades\Hash;
 
 class InvitationRepository
 {
     public function createConfirmationToken(array $data): Confirmation
     {   
         $user_organization = DB::table('user_organizations')->where('user_id', $data['user_id'])->where('organization_id', $data['organization_id'])->first();
-        // $token = Hash::make(time());
         $token = md5(time());
 
         $confirmation = new Confirmation();
